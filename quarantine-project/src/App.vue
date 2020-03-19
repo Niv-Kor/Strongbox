c2dbab1367bf63aeee904ccdbe73383901ad10f2
mordvir7 <mordvir7@gmail.com>
2020-03-19 21:50:21 +0200

    Created main page

--- quarantine-project/src/App.vue
+++ quarantine-project/src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logoo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <MainPage :buttonColor='"blue"'/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    MainPage
+  },
+};
+</script>
---
+++ quarantine-project/src/components/MainPage.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <v-img
+            class='logo'
+            contain
+            src="../assets/logo.png"
+            aspect-ratio="1.5"
+        ></v-img>
+        <v-text-field
+            label="nickname"
+            v-model='nickname'
+            outlined
+            clearable
+            rounded
+        />
+         <v-text-field
+            label ="chat name"
+            v-model='room'
+            outlined
+            clearable
+            rounded
+        />
+        <v-btn
+            class='white--text'
+            :color='buttonColor'
+            fab
+            elevation=2
+        >
+            Go
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nickname: '',
+            room: ''
+        }
+    },
+    computed: {
+        buttonColor() {
+            if (this.isNicknameValid() && this.isRoomValid())
+                return '#76D161'
+            else
+                return '#666666'
+        }
+    },
+    methods: {
+        isNicknameValid: function() {
+            return this.nickname;
+        },
+        isRoomValid: function() {
+            return this.room;
+        },
+    }
+}
+</script>
+
+<style scoped>
+.logo {
+    margin: 3% 10% 3% 10%
+}
+
+.v-text-field {
+    width: 75%;
+    left: 12%;
+}
+
+.v-btn {
+    margin-top: 10%;
+    left: 42.5%;
+}
+</style>
