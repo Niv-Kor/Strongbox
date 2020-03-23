b19a6d3c6de17f04417d675fbb76eba4614769fa
Niv Kor <nivkor23@gmail.com>
2020-03-23 03:24:26 +0200

    Fixed SignUpPage's name

--- Strongbox/src/Routes.js
+++ Strongbox/src/Routes.js
@@ -1,6 +1,6 @@
 import AppCarousel from './components/authentication/AppCarousel';
 import LoginPage from './components/authentication/LoginPage';
-import SignUpPage from './components/authentication/SingUpPage';
+import SignUpPage from './components/authentication/SignUpPage';
 
 export const ROUTES = [
     { path: '/', component: AppCarousel },
