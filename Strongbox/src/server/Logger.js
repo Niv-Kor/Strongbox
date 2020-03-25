4ebae8e12927957024a7fab950cea05a1f01fc91
Niv Kor <nivkor23@gmail.com>
2020-03-25 11:40:44 +0200

    Fixed some server problems

---
+++ Strongbox/src/server/Logger.js
@@ -0,0 +1,9 @@
+const MSG_PREFIX = '>>> '
+const ERR_PREFIX = '\n!---ERROR: '
+const ERR_SUFFIX = '!---\n'
+
+export default {
+    log,
+    error
+};
+
--- Strongbox/src/server/Server.js
+++ Strongbox/src/server/Server.js
@@ -3,12 +3,39 @@ const MSSQL = require('mssql');
 const SERVER = HTTP.createServer();
 const IO = require('socket.io').listen(SERVER);
 const SERVER_PORT = 19200;
+const MSG_PREFIX = '>>> '
+const ERR_PREFIX = '\n!---ERROR: '
+const ERR_SUFFIX = '!---\n'
 const DB_CONFIG = {
     server: 'sql5053.site4now.net',
     database: 'DB_A56D45_Strongbox',
     user: 'DB_A56D45_Strongbox_admin',
     password: 'P2413567cu221',
-    port: 1433
+    port: 1433,
+    options: {
+        encrypt: true,
+        enableArithAbort: true
+    }
+}
+
+/**
+ * Log a valid message to the server's console.
+ * 
+ * @param {String} msg - The message to log
+ */
+function log(msg) { console.log(MSG_PREFIX + msg); }
+
+/**
+ * Log an error message to the server's console.
+ * 
+ * @param {String} msg - A message about the error
+ * @param {Error} err  - The formal error message
+ */
+function logErr(msg, err) {
+    console.error(ERR_PREFIX);
+    if (msg) console.error(msg);
+    console.error(err);
+    console.error(ERR_SUFFIX);
 }
 
 function runQuery(query) {
@@ -38,10 +65,11 @@ IO.on('connection', socket => {
 });
 
 SERVER.listen(SERVER_PORT, function(error) {
-    MSSQL.connect(DB_CONFIG).then(() => {
-        console.log("WOW!");
-    }).catch(err => console.log("aw.."));
-
-    if (error) console.log('Server Startup Error: ' + err);
-    else console.log('Server is listening to port ' + SERVER_PORT);
+    MSSQL.connect(DB_CONFIG)
+        .then(() => {
+            log('Microsoft SQL Server DB is properly connected.');
+            if (error) logErr('Server startup failed.', err);
+            else log('Server is listening to port ' + SERVER_PORT + '.');
+        })
+        .catch(err => logErr('Microsoft SQL Server DB has encountered a problem.', err));
 })
